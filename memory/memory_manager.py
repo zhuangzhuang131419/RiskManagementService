1429cc62e8617d9316a24b9f53ef6cd9f6dcb46f
zhuang <zhengchicheng.zcc@outlook.com>
2024-10-08 09:05:29 +0800

    update

--- memory/memory_manager.py
+++ memory/memory_manager.py
@@ -3,18 +3,33 @@ from model.instrument import Future, Option
 
 class MemoryManager:
     # 指数期货的品种列表
-    Index_Future_ProductIDlist = ['IH', 'IF', 'IM']
+    Index_Future_ProductID = ['IH', 'IF', 'IM']
     # 指数期权的品种列表
-    Index_Option_ProductIDlist = ['HO', 'IO', 'MO']
+    Index_Option_ProductID = ['HO', 'IO', 'MO']
 
     # 期货合约
     futures = []
-
     # 期权合约
     options = []
 
-    # 期权行权价
-    option_month_strike_id = []
+    # 期货月份列表
+    # 3*4 3个品种 每个品种4个月份
+    # eg. [IH2410,IH2411,IH2412,IH2503,IF2410,IF2411,IF2412,IF2503,IM2410,IM2411,IM2412,IM2503]
+    Index_Future_Month_ID = []
+
+    # 期货行情结构
+    # 2*6 time, bid,bidv,ask,askv,avail 代表：字段时间，买1价格，买1量，卖1价格，卖1量，是否可交易标记
+    Index_Future_Price_Data = []
+
+    # 期权品种月份
+    # 3*6 3个品种 每个品种6个月份
+    Index_Option_Month_Forward_ID = []
+    # 期权品种月份行权价个数
+    Index_Option_Month_Strike_Num = []
+
+    # 期权行情结构
+    # 四维的numpy
+    Option_Price_Data = []
 
 
     def __init__(self, instrument_ids):
@@ -23,11 +38,11 @@ class MemoryManager:
 
     # 判断合约是不是在future
     def is_future(self, instrument_id):
-        return any(instrument_id.startswith(future_id) for future_id in self.Index_Future_ProductIDlist)
+        return any(instrument_id.startswith(future_id) for future_id in self.Index_Future_ProductID)
 
     # 判断合约是不是在option
     def is_option(self, instrument_id: str):
-        return any(instrument_id.startswith(option_id) for option_id in self.Index_Option_ProductIDlist)
+        return any(instrument_id.startswith(option_id) for option_id in self.Index_Option_ProductID)
 
 
     def sort_instrument_ids(self):
--- model/instrument.py
+++ model/instrument.py
@@ -36,7 +36,13 @@ class Option(Instrument):
                 f"行权价: {self.strike_price}")
 
 class Future(Instrument):
+
     def __init__(self, instrument_id):
         super().__init__(instrument_id)
         # eg. IH2412
         self.symbol = self.id[:2]
+        self.bid = float
+        self.ask = float
+        self.bid_volume = float
+        self.ask_volume = float
+        self.available = True
